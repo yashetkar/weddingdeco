1b1b05ef4b03d54286c430916dad9bcc7f9dd255
yashetkar <127817218+yashetkar@users.noreply.github.com>
2025-02-13 12:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Background Slider
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
+showSlide(currentSlide);
